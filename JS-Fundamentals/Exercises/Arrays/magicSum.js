29aa2ce9f9254730d4ebd860f6cab9464e332b4b
marians1d <marians1md@gmail.com>
2021-08-23 13:38:13 +0300

    Adding Magic Sum problem solution

---
+++ JS-Fundamentals/Exercises/Arrays/magicSum.js
@@ -0,0 +1,9 @@
+function magicSum(arr, magicNum) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] == magicNum) {
+                console.log(arr[i] + " " + arr[j]);
+            }
+        }
+    }
+}
