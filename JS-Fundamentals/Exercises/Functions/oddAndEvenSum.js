8bef7ea5cd7220167818f336e49e984f6ccc8fcd
marians1d <marians1md@gmail.com>
2021-08-24 14:20:32 +0300

    Adding Odd and Even Sum problem solution

---
+++ JS-Fundamentals/Exercises/Functions/oddAndEvenSum.js
@@ -0,0 +1,16 @@
+function oddAndEvenSum(num) {
+    num = num.toString();
+    let evenSum = 0;
+    let oddSum = 0;
+
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] % 2 === 0) {
+            evenSum += Number(num[i]);
+        } else {
+            oddSum += Number(num[i]);
+        }
+    }
+
+
+    console.log(`Odd sum = ${oddSum}, Even sum = ${evenSum}`);
+}
