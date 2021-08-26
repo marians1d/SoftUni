901b994c3e11de40fe2f00ef3398f72baf8a005b
marians1d <marians1md@gmail.com>
2021-08-26 13:27:34 +0300

    Adding Loading Bar problem solution

---
+++ JS-Fundamentals/Exercises/Functions/loadingBar.js
@@ -0,0 +1,21 @@
+function loadingBar(progressStatus) {
+    let statusMessage = progressStatus + "% ";
+    if (progressStatus !== 100) {
+        statusMessage += "[";
+        for (let i = 0; i < 100; i += 10) {
+            if (progressStatus > i) {
+                statusMessage += "%";
+            } else {
+                statusMessage += ".";
+            }
+        }
+
+        statusMessage += "]";
+
+        console.log(statusMessage);
+        console.log("Still loading...");
+    } else {
+        console.log("100% Complete!");
+        console.log("[%%%%%%%%%%]");
+    }
+}
