b971f1ef0c314252f571cadacc9cdeafd7ac7cce
marians1d <marians1md@gmail.com>
2021-09-02 13:17:02 +0300

    Adding Compacy Users problem solution

---
+++ JS-Fundamentals/Exercises/Associative Arrays/companyUsers.js
@@ -0,0 +1,31 @@
+function solve(input) {
+    let companies = {};
+
+    for (let line of input) {
+        let [name, id] = line.split(' -> ');
+
+        if (companies[name] === undefined) {
+            companies[name] =[];
+        }
+        
+        if (!companies[name].includes(id)) {
+            companies[name].push(id);
+        }
+    }
+    let sortedCompanies = Object.keys(companies).sort((a, b) => a.localeCompare(b));
+
+    for (let key of sortedCompanies) {
+        console.log(key);
+
+        for (let i = 0; i < companies[key].length; i++) {
+            console.log('-- ' + companies[key][i]);
+        }
+    }
+}
+
+solve([
+    'SoftUni -> AA12345',
+    'SoftUni -> BB12345',
+    'Microsoft -> CC12345',
+    'HP -> BB12345'
+])
