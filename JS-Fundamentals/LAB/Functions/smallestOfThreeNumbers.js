f2e3109467c2c478cb8f4bff61a3a074c4d6fd7d
marians1d <marians1md@gmail.com>
2021-09-14 22:28:03 +0300

    Adding Function folder and Smallest of Three Numbers problem solution

---
+++ JS-Fundamentals/LAB/Functions/smallestOfThreeNumbers.js
@@ -0,0 +1,9 @@
+function smallestNum(firstNum, secondNum, thirdNum) {
+    if (firstNum <= secondNum && firstNum <= thirdNum) {
+        console.log(firstNum);
+    } else if (secondNum < firstNum && secondNum < thirdNum) {
+        console.log(secondNum);
+    } else {
+        console.log(thirdNum);
+    }
+}
