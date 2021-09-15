ce613895d678e5480d59da5c47fbcee0feb36d10
marians1d <marians1md@gmail.com>
2021-09-15 13:56:44 +0300

    Adding NxN Matrix problem solution

---
+++ JS-Fundamentals/LAB/Functions/matrix.js
@@ -0,0 +1,9 @@
+function nxnMatrix(n) {
+    for (let i = 0; i < n; i++) {
+        let currentRow = '';
+        for (let j = 0; j < n; j++) {
+            currentRow += n + ' ';
+        }
+        console.log(currentRow);
+    }
+}
