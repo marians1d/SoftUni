eac228dea2546d511714e9db9f33811e06c31a4f
Mariyan Damyanov <marians1md@gmail.com>
2021-09-19 17:24:08 +0300

    Adding Objects and Classes folder and Person Info problem solution

---
+++ JS-Fundamentals/LAB/Objects-and-Classes/city.js
@@ -0,0 +1,7 @@
+function city(cityInfo) {
+    console.log(`name -> ${cityInfo.name}`);
+    console.log(`area -> ${cityInfo.area}`);
+    console.log(`population -> ${cityInfo.population}`);
+    console.log(`country -> ${cityInfo.country}`);
+    console.log(`postCode -> ${cityInfo.postCode}`);
+}
