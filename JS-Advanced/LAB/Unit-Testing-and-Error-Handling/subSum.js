00bacb88d0f3a04c4429c140ad46722f4509ca6b
Mariyan Damyanov <marians1md@gmail.com>
2021-10-10 18:41:07 +0300

    Adding Sub Sum problem solution

---
+++ JS-Advanced/LAB/Unit-Testing-and-Error-Handling/subSum.js
@@ -0,0 +1,24 @@
+function solve(arr, start, end) {
+    // start and end should be inclusive
+    if (!Array.isArray(arr)) {
+        return NaN;
+    }
+
+    if (start < 0) {
+        start = 0;
+    }
+
+    if (end >= arr.length) {
+        end = arr.length - 1;
+    }
+
+    return arr
+            .slice(start, end + 1)
+            .reduce((total, c) => Number(total) + Number(c), 0);
+}
+
+console.log(solve([10, 20, 30, 40, 50, 60], 3, 300));
+console.log(solve([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
+console.log(solve([10, 'twenty', 30, 40], 0, 2));
+console.log(solve([], 1, 2));
+console.log(solve('text', 0, 2));
