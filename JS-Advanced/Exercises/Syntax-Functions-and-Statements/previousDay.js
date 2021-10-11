d316e2ef65e808cf72d907481492588fdd00b41e
Mariyan Damyanov <marians1md@gmail.com>
2021-10-11 22:19:05 +0300

    Adding Previous Day problem solution

---
+++ JS-Advanced/Exercises/Syntax-Functions-and-Statements/previousDay.js
@@ -0,0 +1,8 @@
+function previous(year, month, day) {
+    let date = new Date(year, month - 1, day);
+    date.setDate(date.getDate() - 1);
+    console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+}
+
+previous(2016, 9, 30)
+previous(2016, 10, 1)
