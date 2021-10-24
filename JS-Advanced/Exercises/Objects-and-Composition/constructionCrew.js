8c80bfb712ee63c3eb84dec3af90595a69964793
Mariyan Damyanov <marians1md@gmail.com>
2021-10-24 18:04:45 +0300

    Adding Construction Crew problem solution

---
+++ JS-Advanced/Exercises/Objects-and-Composition/constructionCrew.js
@@ -0,0 +1,10 @@
+function crew(worker) {
+    if (worker.dizziness) {
+        worker.levelOfHydrated += 0.1 * worker.weight * worker.experience;
+        worker.dizziness = false;
+    }
+
+    return worker;
+}
+
+crew({weight: 80, experience: 1, levelOfHydrated: 0, dizziness: true});
