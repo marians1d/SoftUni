deb4b4108cb8926175a7b4df2b37dbea5ea71054
Mariyan Damyanov <marians1md@gmail.com>
2021-10-29 20:19:05 +0300

    Adding Next Article problem solution

---
+++ JS-Advanced/Exercises/Advanced-Functions/08. Next Article/script.js
@@ -0,0 +1,15 @@
+function getArticleGenerator(articles) {
+    let content = document.getElementById('content');
+
+    function showNext() {
+        if (articles.length > 0) {
+        let article = document.createElement('article');
+        console.log(article);
+        article.textContent = articles.shift();
+        
+        content.appendChild(article)
+        }
+    }
+
+    return showNext;
+}
