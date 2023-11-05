be72baaf23f885542bc41aabc2dfe40f97914d00
Mariyan Damyanov <marians1md@gmail.com>
2023-11-05 18:54:44 +0200

    Python Workshop: Adding pets templates

--- Python-Web-Basics/Workshops/petstagram/petstagram/pets/views.py
+++ Python-Web-Basics/Workshops/petstagram/petstagram/pets/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def add_pet(request):
-    pass
+    return render(request, template_name='pets/pet-add-page.html')
 
 
 def pet_details(request, username, pet_slug):
-    pass
+    return render(request, template_name='pets/pet-details-page.html')
 
 
 def edit_pet(request, username, pet_slug):
-    pass
+    return render(request, template_name='pets/pet-edit-page.html')
 
 
 def delete_pet(request, username, pet_slug):
-    pass
+    return render(request, template_name='pets/pet-delete-page.html')
