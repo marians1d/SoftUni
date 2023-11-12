168ce240eb8d95f34394c17ee17fc56b249b1d43
Mariyan Damyanov <marians1md@gmail.com>
2023-11-12 19:15:39 +0200

    Updating pets

--- Python-Web-Basics/Workshops/petstagram/petstagram/pets/views.py
+++ Python-Web-Basics/Workshops/petstagram/petstagram/pets/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
+from petstagram.pets.models import Pet
+
 
 def add_pet(request):
     return render(request, template_name='pets/pet-add-page.html')
 
 
 def pet_details(request, username, pet_slug):
-    return render(request, template_name='pets/pet-details-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+
+    context = {
+        'pet': pet,
+        'all_photos': all_photos,
+    }
+
+    return render(request, template_name='pets/pet-details-page.html', context=context)
 
 
 def edit_pet(request, username, pet_slug):
