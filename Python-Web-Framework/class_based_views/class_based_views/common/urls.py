0326eb16b9a81fe420d2fdc6fc049a86922dba95
Mariyan Damyanov <marians1md@gmail.com>
2024-01-07 17:42:29 +0200

    Add Post model, views, and templates
    A new model named 'Post' was created in 'class_based_views/common/models.py'. Several views were also added in 'class_based_views/common/views.py' to handle the rendering and URL redirection for these posts. Additionally, corresponding HTML templates were added for listing 'Post' objects.

--- Python-Web-Framework/class_based_views/class_based_views/common/urls.py
+++ Python-Web-Framework/class_based_views/class_based_views/common/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.list_posts, name='list_posts'),
+    path('cbv/', views.PostsListView.as_view(), name='cbv_list_posts'),
+    path('cbv/template/', views.PostsTemplateView.as_view(), name='cbv_temp_list_posts'),
+    path('cbv/redirect/', views.PostRedirectView.as_view(), name='cbv_redirect_list_posts'),
+    path('cbv/view/', views.PostListView.as_view(), name='cbv_view_posts')
 ]
--- Python-Web-Framework/class_based_views/class_based_views/common/views.py
+++ Python-Web-Framework/class_based_views/class_based_views/common/views.py
@@ -1,7 +1,47 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.urls import reverse_lazy
+from django.views import generic as views
+
+from class_based_views.common.models import Post
 
 
-# Create your views here.
-class IndexView(View):
-    get(  )
+def list_posts(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+
+    return render(request, 'posts/list.html', context)
+
+
+class PostsListView(views.View):
+    def get(self, *args, **kwargs):
+        context = {
+            'posts': Post.objects.all()
+        }
+
+        return render(self.request, 'posts/list.html', context)
+
+
+class PostsTemplateView(views.TemplateView):
+    template_name = 'posts/list.html'
+
+    # extra_context = {
+    #     'posts': Post.objects.all()
+    # }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all()
+        return context
+
+
+class PostRedirectView(views.RedirectView):
+    url = reverse_lazy('cbv_list_posts')
+
+
+class PostListView(views.ListView):
+    template_name = 'posts/list_view.html'
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 5
+
